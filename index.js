0177acaecb729cd14eb459de1c77440b47ab904c
situ2001 <yongcong2001@outlook.com>
2021-05-11 17:09:23 +0800

    index: use hexo-util to generate HTML tag

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const qrCode = require('yaqrcode');
+const { htmlTag } = require('hexo-util');
 
 hexo.extend.helper.register('renderShare', function() {
     const url = this.url;
     const qrCodeStr = qrCode(url, { size: 150 });
-    const htmlContentQr = `<img src=${qrCodeStr} />`;
-    const htmlContentBrief = `<p>QR code for this article</p>`
-    const htmlContent = `<div align=center>${htmlContentBrief}${htmlContentQr}</div>`;
-    return htmlContent;
+    const attrQr = {
+        src: qrCodeStr,
+        align: "center",
+    };
+    const tagQr = htmlTag('img', attrQr);
+    const tagText = htmlTag('p', {align: "center"}, "QR code for this article");
+    const result = `<div>${tagText}${tagQr}</div>`;
+    return result;
 });
